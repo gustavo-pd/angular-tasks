e69f50d85da893493e6eab2b88fcb96c2415077a
GustavoPD <gudalmoro@gmail.com>
2022-12-01 17:46:26 -0300

    componente dinamico cards

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,7 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
+
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [{
+    conteudo: 'Comunicação entre componentes',
+    autoria: 'Angular',
+    modelo: 'modelo3'
+  },
+  {
+    conteudo: 'Auauau',
+    autoria: 'Cachorro',
+    modelo: 'modelo1'
+  },
+  {
+    conteudo: 'Joga nux peitux do teu pai',
+    autoria: 'Defante',
+    modelo: 'modelo2'
+  }];
 
   constructor() { }
 
